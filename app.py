41e58f6308a3f04129ea6ab99ce1a2d4ddf9b49f
Karen Javadyan <kajarenc@gmail.com>
2025-03-28 18:14:58 +0400

    update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ st.write(
     f"Your real IP address (based on https://checkip.amazonaws.com/) is: {st.context.ip_address}"
 )
 
-x = st.slider("Select a number", 0, 100, 50)
+x = st.slider("Slider for interactivity", 0, 100, 50)
 st.write(f"X squared is {x**2}")
+
+st.write(
+    "WHEEL FILE LINK: https://core-previews.s3-us-west-2.amazonaws.com/pr-10927/streamlit-1.44.0-py3-none-any.whl"
+)
